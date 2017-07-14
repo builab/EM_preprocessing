c2ab2c264b420539526fd3bc3716f5470442792f
builab <huy.bui@mcgill.ca>
2017-07-14 10:37:51 -0400

    Update motioncor2_stack_gpu_parallel.sh

--- motioncor2_stack_gpu_parallel.sh
+++ motioncor2_stack_gpu_parallel.sh
@@ -2,7 +2,12 @@
 # Script to run motioncor2 using GPU for all the stack
 # Base Command for MotionCor2 correction for Falcon 2 movie
 # MotionCor2 -InMrc Micrographs/ABC_movie.mrcs -OutMrc Micrographs/ABC.mrc -LogFile Micrographs/ABC_MotionCor2.log -Patch 5 5 -LogFile ${outDir}/${foo}_MotionCor2.log -StackZ 7 -FmDose 2.3 -InitDose 3.5 -PixSize 1.395 -kV 300 
-inputDir='Movies'
+# Stuff to change
+# FmDose = dose per each bin, normally 50% less than the real dose
+# initDose = dose that included in the frames thrown out in collection
+# PixelSize
+
+inputDir='Frames'
 outDir='Micrographs'
 gpuNo=2
 
@@ -15,10 +20,10 @@ fi
 
 count=0
 
-for i in ${inputDir}/*_movie.mrcs;
+for i in ${inputDir}/*_frames.mrc;
 do
         foo=${i#${inputDir}/}
-        foo=${foo/_movie.mrcs}
+        foo=${foo/_frames.mrc}
         if [ ! -e "${outDir}/${foo}.mrc" ]; then 
                 #echo ${count}
                 #echo MotionCor2 -InMrc ${i} -OutMrc ${outDir}/${foo}.mrc -Patch 5 5 -LogFile ${outDir}/${foo}_MotionCor2.log -StackZ 7 -FmDose 2.3 -InitDose 3.5 -PixSize 1.395 -kV 300 -Gpu ${count} &
