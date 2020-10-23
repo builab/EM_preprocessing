53b13c36d8c0b71fd170e5168699ab2335018592
builab <huy.bui@mcgill.ca>
2020-10-22 21:08:22 -0400

    Update export_e2helixboxer_ptcls.sh

--- export_e2helixboxer_ptcls.sh
+++ export_e2helixboxer_ptcls.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Script to export particle coordinate from e2helixboxer to .box format
 # NOT YET PARALLEL
+# Edit the parameters in the echo & final line of the scripts
 # To export data for a single micrograph
-#  e2helixboxer.py micro.mrc --ptcl-coords=micro.txt
+#  e2helixboxer.py micro.mrc --ptcl-length=120 --ptcl-overlap=90 --ptcl-coords=micro.txt
 
 microDir='Micrographs'
 
@@ -14,5 +15,5 @@ do
                 continue;
         fi
         echo e2helixboxer.py $i --ptcl-coords=${i/.mrc}.txt
-        e2helixboxer.py $i --ptcl-coords=${i/.mrc}.txt
+        e2helixboxer.py $i  --ptcl-length=400 --ptcl-overlap=327 --ptcl-coords=${i/.mrc}.txt
 done; 
